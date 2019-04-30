e3d3c3a7151b72e15b89c79651ec766c497c6955
tassavarat <tassavarat@gmail.com>
2019-04-30 13:57:42 -0700

    Rewrote to pass checker

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print("{:d} is".format(number), end=" ")
 if number > 0:
-    print("positive")
+    print("{:d} is positive".format(number))
 elif number == 0:
-    print("zero")
+    print("{:d} is zero".format(number))
 else:
-    print("negative")
+    print("{:d} is negative".format(number))
