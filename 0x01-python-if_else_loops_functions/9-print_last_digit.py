622c56803218534d6c4b466b243b206f6be64f5e
tassavarat <tassavarat@gmail.com>
2019-04-30 18:19:16 -0700

    First commit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+def print_last_digit(number):
+    if number > 0:
+        lastdigit = number % 10
+    else:
+        lastdigit = -number % 10
+    print("{:d}".format(lastdigit), end="")
+    return lastdigit
