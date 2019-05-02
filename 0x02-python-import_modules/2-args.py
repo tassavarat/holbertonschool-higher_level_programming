0f7d98a2510e3f15971603c72623a98e0bd4ab52
tassavarat <tassavarat@gmail.com>
2019-05-02 16:08:07 -0700

    Fixed output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     arguments = len(argv) - 1
     i = 1
-    if arguments is 1:
+    if arguments is 0:
         print("{:d} argument.".format(arguments))
+    elif arguments is 1:
+        print("{:d} argument:".format(arguments))
     else:
         print("{:d} arguments:".format(arguments))
         for i, args in enumerate(argv[i:], 1):
