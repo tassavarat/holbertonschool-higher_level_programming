b23269466cf04efae99069df0424229f2aa00883
tassavarat <tassavarat@gmail.com>
2019-05-07 15:43:41 -0700

    Rewrote without loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx >= 0 and idx < len(my_list):
-        for i in range(len(my_list)):
-            if i is idx:
-                return my_list[i]
+        return my_list[idx]
     return None
