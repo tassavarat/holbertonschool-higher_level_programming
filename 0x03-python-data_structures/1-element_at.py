785edd363331ea576e2aecbe7682dac5f1b40432
tassavarat <tassavarat@gmail.com>
2019-05-07 15:19:10 -0700

    First commit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if i >= 0:
+        for i in range(len(my_list)):
+            if i is idx:
+                return my_list[i]
+    return None
