30f6de8376d4d4e211fe33a2fef8cb03b20333a0
tassavarat <tassavarat@gmail.com>
2019-05-07 15:24:11 -0700

    Added conditional for return None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if i >= 0:
+    if idx >= 0 and idx < len(my_list):
         for i in range(len(my_list)):
             if i is idx:
                 return my_list[i]
