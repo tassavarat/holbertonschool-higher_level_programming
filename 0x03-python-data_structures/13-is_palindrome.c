a8e75ab928b2ae508e0d21b21510ed6e6bb77fd9
tassavarat <tassavarat@gmail.com>
2019-05-07 23:07:58 -0700

    First commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a linked list
+ * @head: Pointer to address of a list
+ *
+ * Return: Pointer to first node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp, *cur, *rev;
+
+	tmp = cur = *head;
+	rev = NULL;
+	while (cur)
+	{
+		tmp = tmp->next;
+		cur->next = rev;
+		rev = cur;
+		cur = tmp;
+	}
+	return (rev);
+}
+
+/**
+ * is_palindrome - Checks if singly linked list is palindrome
+ * @head: Pointer to pointer of first node
+ *
+ * Return: 1 if palindrome or 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast, *rev, *cur;
+
+	fast = slow = cur = *head;
+	if (*head)
+	{
+		while (fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		/**
+		 * printf("slow->n is %d\n", slow->n);
+		 * printf("slow->n is %d\n", slow->next->n);
+		*/
+		rev = reverse_listint(&slow);
+		/**
+		 *
+		 * print_listint(rev);
+		 * printf("###############\n");
+		 * print_listint(*head);
+		*/
+		while (cur && rev)
+		{
+			if (cur->n != rev->n)
+				return (0);
+			cur = cur->next;
+			rev = rev->next;
+		}
+	}
+	return (1);
+}
