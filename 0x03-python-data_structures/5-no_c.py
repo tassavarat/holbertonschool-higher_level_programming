9b670d0f0ac9cad39471a769e106ba4186246b91
tassavarat <tassavarat@gmail.com>
2019-05-07 17:33:02 -0700

    First commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        s = ""
+        for c in my_string:
+            if c == 'c' or c == 'C': 
+                continue
+            else:
+                s += c
+        return s
