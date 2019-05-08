2ed911ba6c03774d3ce019e2dcdf772b58071e0d
tassavarat <tassavarat@gmail.com>
2019-05-07 21:00:26 -0700

    Reformat code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     list_result = []
-    for i, num in enumerate(my_list):
+    for num in (my_list):
         if num % 2 == 0:
             list_result.append(True)
         else:
