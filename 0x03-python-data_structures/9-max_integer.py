f7b50fe40e2df26912c07c7cd0c7211611e8920a
tassavarat <tassavarat@gmail.com>
2019-05-07 20:37:48 -0700

    Rewrote logic

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        large = 0
+        large = my_list[0]
         for i in my_list:
-            if i >= large:
+            if large < i:
                 large = i
         return large
     return None
