baf90cd503bebd3dfc5476d7f1e8fc9c15ca9863
tassavarat <tassavarat@gmail.com>
2019-05-07 19:07:02 -0700

    Added integer specifier

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in (matrix):
         for j in i:
-            print("{}".format(j), end='')
+            print("{:d}".format(j), end='')
             if j != i[-1]:
                 print("".format(), end=' ')
         print()
