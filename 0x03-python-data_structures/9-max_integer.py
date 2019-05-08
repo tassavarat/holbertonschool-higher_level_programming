b86e0caedfbb03cd82d05ff0fd6e3a3578cf104e
tassavarat <tassavarat@gmail.com>
2019-05-08 14:29:29 -0700

    Refactored code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list:
         large = my_list[0]
-        for i in my_list:
+        for i in my_list[1:]:
             if large < i:
                 large = i
         return large
