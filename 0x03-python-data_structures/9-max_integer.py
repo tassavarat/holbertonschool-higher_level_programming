57a74064300cdb886dbbd6b098564b8d0deea9ff
tassavarat <tassavarat@gmail.com>
2019-05-07 20:24:25 -0700

    First commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+def max_integer(my_list=[]):
+    if my_list:
+        large = 0
+        for i in my_list:
+            if large < i:
+                large = i
+        return large
+    return None
