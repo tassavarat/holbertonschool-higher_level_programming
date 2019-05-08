fcf864b6a094817e2e993a50e989d5810df6b3f4
tassavarat <tassavarat@gmail.com>
2019-05-07 17:46:43 -0700

    Debug changes

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        s = ""
-        for c in my_string:
-            if c == 'c' or c == 'C':
-                continue
-            else:
-                s += c
-        return s
-    return ""
+    s = ""
+    for c in my_string:
+        if c == 'c' or c == 'C':
+            continue
+        else:
+            s += c
+    return s
