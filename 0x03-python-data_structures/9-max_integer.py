c9f530c7583c1d82492eae779a083dd79167e00e
tassavarat <tassavarat@gmail.com>
2019-05-07 20:31:20 -0700

    Delete

--- 0x03-python-data_structures/9-max_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def max_integer(my_list=[]):
-    if my_list:
-        large = 0
-        for i in my_list:
-            if large < i:
-                large = i
-        return large
-    return None
