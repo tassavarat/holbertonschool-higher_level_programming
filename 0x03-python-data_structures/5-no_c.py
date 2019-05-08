088ceb14c7bfa60d512cf193e91aab7eb552fdad
tassavarat <tassavarat@gmail.com>
2019-05-07 17:34:14 -0700

    Removed trailing whitespace

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     if my_string:
         s = ""
         for c in my_string:
-            if c == 'c' or c == 'C': 
+            if c == 'c' or c == 'C':
                 continue
             else:
                 s += c
