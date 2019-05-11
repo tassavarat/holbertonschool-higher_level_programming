a4ae7c445cc23a81abb8337a593eec1576e22458
tassavarat <tassavarat@gmail.com>
2019-05-10 17:35:11 -0700

    Refactored code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,36 +1,14 @@
 #!/usr/bin/python3
-def roman(r):
-    if r == 'I':
-        return 1
-    if r == 'V':
-        return 5
-    if r == 'X':
-        return 10
-    if r == 'L':
-        return 50
-    if r == 'C':
-        return 100
-    if r == 'D':
-        return 500
-    if r == 'M':
-        return 1000
-    return 0
-
-
 def roman_to_int(roman_string):
-    i = 0
-    d = 0
-    while isinstance(roman_string, str) and i < len(roman_string):
-        s1 = roman(roman_string[i])
-        if i + 1 < len(roman_string):
-            s2 = roman(roman_string[i + 1])
-            if s1 >= s2:
-                d += s1
-                i += 1
-            else:
-                d = d + s2 - s1
-                i += 2
-        else:
-            d += s1
-            i += 1
-    return d
+    res = 0
+    if type(roman_string) == str:
+        largest = 1
+        rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for s in roman_string[::-1]:
+            if s in rn:
+                if rn[s] >= largest:
+                    res = res + rn[s]
+                    largest = rn[s]
+                else:
+                    res -= rn[s]
+    return res
