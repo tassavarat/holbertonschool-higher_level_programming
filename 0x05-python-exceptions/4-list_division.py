35d4eb580637dd88159a3fcda6626458f2e619e1
tassavarat <tassavarat@gmail.com>
2019-05-20 15:50:43 -0700

    Fixed typo

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             tmp = 0
         except IndexError:
-            print("out or range")
+            print("out of range")
             tmp = 0
         finally:
             result.append(tmp)
