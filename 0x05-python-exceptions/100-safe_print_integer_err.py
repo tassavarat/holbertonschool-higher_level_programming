83f8012971aac7c41e5852b5da74ea6b9ecfd590
tassavarat <tassavarat@gmail.com>
2019-05-20 17:00:05 -0700

    First commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as fe:
+        print("Exception:", fe, file=stderr)
+        return False
