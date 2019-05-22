d31482be1f1fea63a1db237f2f7005008688dd06
tassavarat <tassavarat@gmail.com>
2019-05-21 18:17:28 -0700

    First commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,10 +4,6 @@ class Square:
     def __init__(self, size=0):
         """Initialises the data"""
         self.__size = size
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
     def area(self):
         """Returns current square area"""
@@ -22,3 +18,7 @@ class Square:
     def size(self, value):
         """Setter method"""
         self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
