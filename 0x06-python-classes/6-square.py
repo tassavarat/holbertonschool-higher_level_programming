aba5382589eab6700e83522f32faa446eb38e4c4
tassavarat <tassavarat@gmail.com>
2019-05-21 19:59:21 -0700

    Added more conditionals

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,6 +46,10 @@ class Square:
     @position.setter
     def position(self, value):
         """Setter method"""
-        if type(value) != int and (value[0] < 0 and value[1] < 0):
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
