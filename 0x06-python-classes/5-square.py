1f930f5324387fda46866a22c8739c200ab032ec
tassavarat <tassavarat@gmail.com>
2019-05-21 19:41:07 -0700

    Refactored code

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -17,11 +17,11 @@ class Square:
     @size.setter
     def size(self, value):
         """Setter method"""
-        self.__size = value
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def my_print(self):
         """Prints the square"""
