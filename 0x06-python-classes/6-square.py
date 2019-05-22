2aa689f579d1672aa1a992e18c8f8ef19197b163
tassavarat <tassavarat@gmail.com>
2019-05-21 19:48:06 -0700

    Changed conditionals

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,16 +26,17 @@ class Square:
 
     def my_print(self):
         """Prints the square"""
-        for y in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            for x in range(self.__position[0]):
-                print(' ', end='')
-            for j in range(self.__size):
-                print('#', end='')
-            print()
         if self.__size == 0:
             print()
+        else:
+            for y in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(' ', end='')
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
 
     @property
     def position(self):
