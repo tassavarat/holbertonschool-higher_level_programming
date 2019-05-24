c954365b89fadf2fef83cbaea6cac6f0a7c0d131
tassavarat <tassavarat@gmail.com>
2019-05-23 21:06:52 -0700

    First commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Adds two integers"""
+    if type(a) is float:
+        a = int(a)
+    elif type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is float:
+        b = int(b)
+    elif type(b) is not int:
+        raise TypeError("b must be an integer")
+    return a + b
