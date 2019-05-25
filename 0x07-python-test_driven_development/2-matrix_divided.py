bb2c4b329fee0d677d96d0f4c63120079fbdfad5
tassavarat <tassavarat@gmail.com>
2019-05-24 20:39:12 -0700

    Added NaN

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,7 +15,7 @@ def matrix_divided(matrix, div):
     else:
         raise TypeError("matrix must be a matrix (list of lists)\
  of integers/floats")
-    if type(div) is not int and type(div) is not float:
+    if type(div) is not int and type(div) is not float or div != div:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
