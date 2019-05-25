7ee9a8a21bbf68dcaefe89847a48140fa85a1f40
tassavarat <tassavarat@gmail.com>
2019-05-24 19:00:25 -0700

    First commit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,7 +15,6 @@ def matrix_divided(matrix, div):
     else:
         raise TypeError("matrix must be a matrix (list of lists)\
  of integers/floats")
-
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_square(size):
+    """Prints a square with the character #"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        print('', end='')
+    else:
+        for _ in range(size):
+            for _ in range(size):
+                print('#', end='')
+            print()
