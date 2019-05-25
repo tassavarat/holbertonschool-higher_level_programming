4156ec8fe3c4e4f4d36492434c1c8dcc6a875344
tassavarat <tassavarat@gmail.com>
2019-05-24 21:48:15 -0700

    First commit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """Prints a text with 2 new lines after ., ?, and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    delimiter = ['.', '?', ':']
+    s = ""
+
+    for c in text:
+        s += c
+        if c in delimiter:
+            print(s.strip())
+            print()
+            s = ""
+    print(s.strip(), end='')
