6a7797070320e1c6e0e126c6230fa5591868faf6
tassavarat <tassavarat@gmail.com>
2019-05-28 16:30:27 -0700

    First commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """Instantiation
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter method
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """"Setter method
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Rectangle area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Rectangle perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
