836dc7af1e20abca687ae2c72af89fa8e8435963
tassavarat <tassavarat@gmail.com>
2019-06-06 00:22:23 -0700

    Changed negation

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,4 +3,4 @@ def inherits_from(obj, a_class):
     """Returns True if obj is an instance of a class that inherited \
 from the specified class; otherwise False
     """
-    return not type(obj) is a_class and isinstance(obj, a_class)
+    return type(obj) is not a_class and isinstance(obj, a_class)
