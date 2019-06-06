c9fec28f3ef6a5603791391d63066f537fdef4bf
tassavarat <tassavarat@gmail.com>
2019-06-06 00:24:09 -0700

    Changed return order

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,4 +3,4 @@ def inherits_from(obj, a_class):
     """Returns True if obj is an instance of a class that inherited \
 from the specified class; otherwise False
     """
-    return type(obj) is not a_class and isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
