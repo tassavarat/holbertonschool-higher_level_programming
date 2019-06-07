b7366e218de074b4ac8cc97111f983a2aca80e6c
tassavarat <tassavarat@gmail.com>
2019-06-06 23:35:05 -0700

    First commit

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""14-pascal_triangle module
+"""
+
+def factorial(n):
+    """Returns factorialorial
+    """
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial = factorial * i
+    return factorial
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal's triangle
+    of n
+    """
+    res = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            row.append(factorial(i) // (factorial(j) * factorial(i - j)))
+        res.append(row)
+    return res
