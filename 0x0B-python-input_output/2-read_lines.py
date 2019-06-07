d1e56974f6aa1db5fc7f6a791df9affc28187c3b
tassavarat <tassavarat@gmail.com>
2019-06-06 18:18:56 -0700

    Fixed logic

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,7 +7,7 @@ def read_lines(filename="", nb_lines=0):
     """Reads n lines of a text file (UTF8) and prints to stdout
     """
     with open(filename, encoding="utf8") as f:
-        if not nb_lines:
+        if nb_lines <= 0:
             print(f.read(), end='')
         else:
             for line in range(nb_lines):
