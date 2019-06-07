430767c98ed2e134482a145e1baadcfa1f389ed4
tassavarat <tassavarat@gmail.com>
2019-06-06 21:20:35 -0700

    First commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""12-student method
+"""
+
+
+class Student:
+    """Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of Student instance
+        """
+        if attrs:
+            my_dict = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict[i] = self.__dict__[i]
+            return my_dict
+        return self.__dict__
