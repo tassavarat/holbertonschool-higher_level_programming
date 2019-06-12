e81746b6531fd2991a87648dbe207a66b977d776
tassavarat <tassavarat@gmail.com>
2019-06-12 12:01:05 -0700

    Added additional if

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,7 +57,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns: JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
