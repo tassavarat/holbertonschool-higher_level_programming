91763c03b0f843d385f2597b2dc218e97972f92c
tassavarat <tassavarat@gmail.com>
2019-06-13 14:33:29 -0700

    Added test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -113,13 +113,6 @@ class TestBase(unittest.TestCase):
                             "x": 0, "id": 7, "width": 2, "y": 0, "height": 4}]
                         )))
 
-    def test_save_to_file_empty(self):
-        """Testing JSON string rep empty"""
-        Rectangle.save_to_file([])
-        with open("Rectangle.json") as file:
-            self.assertEqual(
-                    (file.read()), "[]")
-
     def test_save_to_file_len_Square(self):
         """Testing JSON string rep len"""
         r1 = Square(10, 7, 2, 8)
