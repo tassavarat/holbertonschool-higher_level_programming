0d4efb00534ed27f227cb6cf1d4ff307638b49cd
tassavarat <tassavarat@gmail.com>
2019-07-15 16:49:58 -0700

    Explicitly set id

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -13,7 +13,7 @@
 
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256), score INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, "John", "10");
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, "Alex", "3");
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, "Bob", "14");
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, "George", "8");
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", "10");
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", "3");
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", "14");
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", "8");
