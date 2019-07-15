e9810564d7f67adea6b07e814031d8208e7c3f22
tassavarat <tassavarat@gmail.com>
2019-07-15 14:48:34 -0700

    4 implementation

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+-- Creates a table called first_table in the current database
+-- first_table description:
+-- 	id INT
+--	name VARCHAR(256)
+-- Database name will be passed as an argument of the mysql command
+-- If first_table already exists, script should not fail
+-- Do not use SELECT or SHOW
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
