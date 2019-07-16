fa77cec596723f2a097fc421af506868b551d842
tassavarat <tassavarat@gmail.com>
2019-07-16 15:17:09 -0700

    First commit

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Creates MySQL server user_0d_1 with all privileges
+-- Password should be user_0d_1_pwd 
+-- If user already exists, script should not fail
+
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY password;
