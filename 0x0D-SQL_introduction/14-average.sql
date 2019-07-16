7585acb7f5f2a02c735690ea349ecd865086e5a5
tassavarat <tassavarat@gmail.com>
2019-07-15 17:48:47 -0700

    14 solution

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,8 @@
+-- Computes the score average of all records in table second_table
+-- of specified database
+-- Result column name should be average
+
+SELECT
+	AVG(score) 'average'
+FROM
+	second_table;
