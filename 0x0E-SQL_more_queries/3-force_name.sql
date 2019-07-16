3a1c788dce0e3da9c34c00f8c5df8eec76ecde73
tassavarat <tassavarat@gmail.com>
2019-07-16 15:52:58 -0700

    First commit

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- Creates the table force_name
+-- force_name description:
+--	id INT
+--	name VARCHAR(256) can't be null
+-- If the table already exists, the script should not fail
+
+CREATE TABLE IF NOT EXISTS force_name (id INT,
+name VARCHAR(256) NOT NULL);
