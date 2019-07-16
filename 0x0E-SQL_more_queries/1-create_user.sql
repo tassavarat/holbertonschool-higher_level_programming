4bec3f109aadef5010906b7fda280eddb9cac36c
tassavarat <tassavarat@gmail.com>
2019-07-16 15:25:10 -0700

    Changed syntax and added privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,6 @@
 -- Password should be user_0d_1_pwd 
 -- If user already exists, script should not fail
 
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY password;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
