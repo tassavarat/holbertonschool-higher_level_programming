955f8991a8461bdcc18530350e6e45e6eba3146a
tassavarat <tassavarat@gmail.com>
2019-07-15 17:58:17 -0700

    15 implementation

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,15 @@
+-- Lists the number of records with the same score in the table second_table
+-- of the specified database
+-- Results should display:
+-- 	score
+--	number of records for this score with the label number
+-- Sorted in descending order of number of records
+
+SELECT
+	score, count(*)
+FROM
+	second_table
+GROUP BY
+	score
+ORDER BY
+	score DESC;
