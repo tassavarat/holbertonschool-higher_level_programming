29f1e5f90d20f1b60f69873a910767c319deb089
tassavarat <tassavarat@gmail.com>
2019-07-16 16:29:36 -0700

    First commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,13 @@
+-- Creates the database hbtn_0d_usa and the table states
+-- states description
+--	id INT unique, auto generated, can't be null and is primary key
+--	name VARCHAR(256) can't be null
+-- Should not fail if database or states exists 
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
