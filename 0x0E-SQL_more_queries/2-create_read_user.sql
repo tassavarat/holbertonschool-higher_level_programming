be7ab5815aa637d65b8020c8c93d5963cbb36899
tassavarat <tassavarat@gmail.com>
2019-07-16 15:41:27 -0700

    Specified table

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,4 +6,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT * . * TO 'user_0d_1'@'localhost'
+GRANT SELECT ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
