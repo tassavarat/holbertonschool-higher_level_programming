3adf300105790f0ea7e4c66e423d6c51ce1e1128
tassavarat <tassavarat@gmail.com>
2019-07-16 16:03:12 -0700

    First commit

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,11 @@
+-- Creates the table id_not_null
+-- id_not_null description:
+--	id INT with default value 1
+--	name VARCHAR(256)
+-- Script should not fail if table exists
+
+CREATE TABLE IF NOT EXISTS id_not_null
+(
+	id INT DEFAULT 1
+	name VARCHAR(256)
+);
