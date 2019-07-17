9f38f7a8fb307312501ded783ca61b397eed6ed2
tassavarat <tassavarat@gmail.com>
2019-07-17 14:05:41 -0700

    First commit

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,19 @@
+-- Lists all genres not linked to the show Dexter
+
+SELECT DISTINCT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name NOT IN
+(
+	SELECT DISTINCT tv_genres.name
+	FROM tv_genres
+	INNER JOIN tv_show_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter"
+)
+ORDER BY name ASC;
