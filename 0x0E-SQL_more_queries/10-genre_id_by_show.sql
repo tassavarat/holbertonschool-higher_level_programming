99a864140410f1b68c7225a5f0c37cf3b79b36af
tassavarat <tassavarat@gmail.com>
2019-07-16 19:33:32 -0700

    First commit

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Lists all shows in hbtn_0d_tvshows with at least one genre linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title ASC;
