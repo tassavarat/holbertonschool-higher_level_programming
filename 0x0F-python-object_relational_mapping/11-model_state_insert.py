85c200bab42f8dd08c1d7b2857522e081979434e
tassavarat <tassavarat@gmail.com>
2019-08-10 17:37:07 -0700

    First commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""11-model_state_insert
+Adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State, Base
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(State(name="Louisiana"))
+    session.commit()
+    for data in session.query(State).filter_by(name="Louisiana"):
+        print(data.id)
+    session.close()
