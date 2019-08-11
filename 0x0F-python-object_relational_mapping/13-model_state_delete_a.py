768e9b4d30726afece8c674cdbb772e86b9e0953
tassavarat <tassavarat@gmail.com>
2019-08-10 18:59:16 -0700

    First commit

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""12-model_state_update_id_2
+Changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State, Base
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(State).filter(State.name.like("%a%")).\
+        delete(synchronize_session=False)
+    session.commit()
+    session.close()
