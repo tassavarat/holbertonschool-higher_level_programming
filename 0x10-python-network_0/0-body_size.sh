ce08b256ca73e9e06502ea744a84d0c123528cae
tassavarat <tassavarat@gmail.com>
2019-08-20 14:09:55 -0700

    Reduced wc -l to 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the
-# body of the response
-
+# Takes URL and displays the size of the body of the response
 curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
