9b0f0a26703589a043341f7f222178336b911185
tassavarat <tassavarat@gmail.com>
2019-08-20 19:23:02 -0700

    Rewrote for more efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,11 +2,12 @@
 """6-peak module"""
 
 
+"""
 def find_peak(list_of_integers):
-    """Finds a peak
+    Finds a peak
     list_of_integers - Given values
     Return: Peak
-    """
+
     length = len(list_of_integers)
     i = length//2
     # if i != 0:
@@ -16,11 +17,39 @@ def find_peak(list_of_integers):
     if i == 0:
         return None
     elif list_of_integers[i] < list_of_integers[i - 1]:
-        while list_of_integers[i - 1] > list_of_integers[i]:
+        while i > 0 and list_of_integers[i - 1] > list_of_integers[i]:
             # print("left i is", list_of_integers[i])
             i = i - 1
     elif list_of_integers[i] < list_of_integers[i + 1]:
-        while i < length - 1 and list_of_integers[i + 1] > list_of_integers[i]:
+        while i + 1 < length and list_of_integers[i + 1] > list_of_integers[i]:
             # print("right i is", list_of_integers[i])
             i = i + 1
     return list_of_integers[i]
+"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak
+    list_of_integers - Given values
+    Return: Peak
+    """
+
+    length = len(list_of_integers)
+    mid = length
+    i = length//2
+
+    if i == 0:
+        return None
+
+    while True:
+        mid = mid//2
+        if i > 0 and list_of_integers[i] < list_of_integers[i - 1]:
+            if mid//2 == 0:
+                mid = 2
+            i = i - mid//2
+        elif i + 1 < length and list_of_integers[i] < list_of_integers[i + 1]:
+            if mid//2 == 0:
+                mid = 2
+            i = i + mid//2
+        else:
+            return list_of_integers[i]
