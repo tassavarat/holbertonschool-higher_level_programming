c2dff3eef1716793b355c2d799f6b60e0b0709e0
tassavarat <tassavarat@gmail.com>
2019-08-20 17:32:17 -0700

    Fixed index error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,14 +7,20 @@ def find_peak(list_of_integers):
     list_of_integers - Given values
     Return: Peak
     """
-    i = len(list_of_integers)//2
+    length = len(list_of_integers)
+    i = length//2
+    # if i != 0:
+    # print("list_of_integers is", list_of_integers)
+    # print("list_of_integers[{}] is {}".format(i, list_of_integers[i]))
 
     if i == 0:
         return None
     elif list_of_integers[i] < list_of_integers[i - 1]:
         while list_of_integers[i - 1] > list_of_integers[i]:
+            # print("left i is", list_of_integers[i])
             i = i - 1
     elif list_of_integers[i] < list_of_integers[i + 1]:
-        while list_of_integers[i + 1] > list_of_integers[i]:
+        while i < length - 1 and list_of_integers[i + 1] > list_of_integers[i]:
+            # print("right i is", list_of_integers[i])
             i = i + 1
     return list_of_integers[i]
