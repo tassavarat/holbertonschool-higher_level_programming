f3b78474dbec23719879dcb4ebaf699b4e8f184e
tassavarat <tassavarat@gmail.com>
2019-08-20 19:59:44 -0700

    Fixed formatting

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,19 +10,19 @@ def find_peak(list_of_integers):
 
     length = len(list_of_integers)
     mid = length
-    i = length//2
+    i = length // 2
 
     if i == 0:
         return None
 
     while True:
-        mid = mid//2
+        mid = mid // 2
         if i > 0 and list_of_integers[i] < list_of_integers[i - 1]:
-            if mid//2 == 0:
+            if mid // 2 == 0:
                 mid = 2
             i = i - mid//2
         elif i + 1 < length and list_of_integers[i] < list_of_integers[i + 1]:
-            if mid//2 == 0:
+            if mid // 2 == 0:
                 mid = 2
             i = i + mid//2
         else:
