d46f8019f940fc7eaa169887086ce4162737f0e8
tassavarat <tassavarat@gmail.com>
2019-08-26 15:48:56 -0700

    Prevent execution when import

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status using requests package"""
-import requests
+if __name__ == "__main__":
+    import requests
 
-r = requests.get("https://intranet.hbtn.io/status")
-print("Body response:\n"
-      "\t- type: {}\n"
-      "\t- content: {}".format(type(r.text), r.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:\n"
+          "\t- type: {}\n"
+          "\t- content: {}".format(type(r.text), r.text))
