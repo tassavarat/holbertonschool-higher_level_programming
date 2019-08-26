3c5311230c3131d8b088736f83811aba660de377
tassavarat <tassavarat@gmail.com>
2019-08-26 14:07:11 -0700

    Stops execution when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
-from urllib import request
+if __name__ == "__main__":
+    from urllib import request
 
-with request.urlopen("https://intranet.hbtn.io/status") as f:
-    status = f.read()
-    print("Body response:\n"
-          "\t- type: {}\n"
-          "\t- content: {}\n"
-          "\t- utf8 content: {}".format(type(status), status,
-                                        status.decode("utf-8")))
+    with request.urlopen("https://intranet.hbtn.io/status") as f:
+        status = f.read()
+        print("Body response:\n"
+              "\t- type: {}\n"
+              "\t- content: {}\n"
+              "\t- utf8 content: {}".format(type(status), status,
+                                            status.decode("utf-8")))
