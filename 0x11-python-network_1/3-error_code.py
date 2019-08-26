89d3406e2106f12d20b2f0ba2e8acc19ecaee9f8
tassavarat <tassavarat@gmail.com>
2019-08-26 15:33:12 -0700

    First commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+"""Takes URL, sends request to URL, displays body of response decoded in utf-8
+"""
+if __name__ == "__main__":
+    from urllib import request, error
+    from sys import argv
+
+    try:
+        with request.urlopen(argv[1]) as resp:
+            print(resp.read().decode("utf-8"))
+    except error.HTTPError as e:
+        print("Error code:", e.code)
