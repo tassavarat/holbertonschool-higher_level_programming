4fcc4de5858ee57d1c77d358aa0f6ddfdd7631a6
tassavarat <tassavarat@gmail.com>
2019-08-26 19:09:40 -0700

    First commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes a letter and sends POST request to http://0.0.0.0:5000/search_user
+with letter as parameter
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) == 1:
+        data = {"q": ""}
+    else:
+        data = {"q": argv[1]}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data).json()
+    if not isinstance(r, dict):
+        print("Not a valid JSON")
+    elif r == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(r["id"], r["name"]))
