23fe2f5d6aa0a4cbc5080c28a4206fae1130e6fc
tassavarat <tassavarat@gmail.com>
2019-08-26 19:26:59 -0700

    Check header for invalid json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,11 +11,10 @@ if __name__ == "__main__":
     else:
         data = {"q": argv[1]}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=data).json()
-    try:
-        if r == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(r["id"], r["name"]))
-    except Exception:
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    if r.headers.get("content-type") != "application/json":
         print("Not a valid JSON")
+    elif r.json() == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(r.json()["id"], r.json()["name"]))
