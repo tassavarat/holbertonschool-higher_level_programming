1534211b87cba34e8bb16d486be88c7cca8dc163
tassavarat <tassavarat@gmail.com>
2019-08-30 10:29:16 -0700

    First commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    r = requests.get("https://api.github.com/repos/{}/{}/commits".
+                     format(argv[2], argv[1])).json()
+    length = len(r)
+
+    if length > 10:
+        for i in range(10):
+            print("{}: {}".format(r[i].get("sha"), r[i].get("commit").
+                                  get("author").get("name")))
+    else:
+        for i in range(length):
+            print("{}: {}".format(r[i].get("sha"), r[i].get("commit").
+                                  get("author").get("name")))
