9230e4714d0b54bac6f9d14c027a6dcc899ded8f
Tim Assavarat <tassavarat@gmail.com>
2019-09-17 22:00:45 +0000

    First commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* Prints 3 lines by using an array of strings and a loop */
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; array[i]; ++i) {
+  console.log(array[i]);
+}
