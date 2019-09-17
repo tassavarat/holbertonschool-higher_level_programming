0b2c4a7b5e3a613506493db298556a46f207804d
Tim Assavarat <tassavarat@gmail.com>
2019-09-17 21:20:33 +0000

    First commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* Prints a message depending on number of aruments passed */
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
