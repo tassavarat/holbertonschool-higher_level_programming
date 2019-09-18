d9be112c71b8fdcbd168ed0ed0972f453884900b
Tim Assavarat <tassavarat@gmail.com>
2019-09-18 01:45:39 +0000

    Properly handled negatives

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
 
 /* Searches the second biggest int int list of arguments */
-if (!process.argv[2] || process.argv.length === 3) {
+if (process.argv.length === 2 || process.argv.length === 3) {
   console.log('0');
 } else {
-  const l = Math.max.apply(Math, process.argv.slice(2));
-  let sl = process.argv[2];
+  const l = Math.max.apply(null, process.argv.slice(2));
+  let sl;
+  if (process.argv[2] !== l) {
+    sl = parseInt(process.argv[2]);
+  } else {
+    sl = parseInt(process.argv[3]);
+  }
 
   for (let i = 3; process.argv[i]; ++i) {
     if (sl < process.argv[i] && process.argv[i] < l) {
