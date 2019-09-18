23b65f682ff6c983dd34f30279b3a92644a9ca97
Tim Assavarat <tassavarat@gmail.com>
2019-09-18 04:19:24 +0000

    Rewrote without loop

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,18 +4,9 @@
 if (process.argv.length === 2 || process.argv.length === 3) {
   console.log('0');
 } else {
-  const l = Math.max.apply(null, process.argv.slice(2));
-  let sl;
-  if (parseInt(process.argv[2]) !== l) {
-    sl = parseInt(process.argv[2]);
-  } else {
-    sl = parseInt(process.argv[3]);
-  }
+  const array = process.argv.slice(2).sort((a ,b) => a - b);
+  const len = array.length;
+  const max = Math.max.apply(null, process.argv.slice(2));
 
-  for (let i = 3; process.argv[i]; ++i) {
-    if (sl < process.argv[i] && process.argv[i] < l) {
-      sl = process.argv[i];
-    }
-  }
-  console.log(String(sl));
+  console.log(array[len - 2]);
 }
