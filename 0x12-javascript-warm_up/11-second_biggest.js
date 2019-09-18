9e7a80ca464b5320f889bf5b2ea3343a33616c78
Tim Assavarat <tassavarat@gmail.com>
2019-09-18 01:04:18 +0000

    Always print out string

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 
 /* Searches the second biggest int int list of arguments */
-let sl = 0;
-
 if (!process.argv[2] || process.argv.length === 3) {
-  console.log(sl);
+  console.log('0');
 } else {
   const l = Math.max.apply(null, process.argv.slice(2));
+  let sl = 0;
 
   for (let i = 2; process.argv[i]; ++i) {
     if (sl < process.argv[i] && process.argv[i] < l) {
       sl = process.argv[i];
     }
   }
-  console.log(String(sl));
+  console.log(sl);
 }
