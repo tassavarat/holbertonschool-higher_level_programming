6f65e78f3c8898690495abeb15640fc0074ee7db
Tim Assavarat <tassavarat@gmail.com>
2019-09-18 01:02:35 +0000

    Semistandard compliance

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@
 /* Searches the second biggest int int list of arguments */
 let sl = 0;
 
-if (!process.argv[2] || process.argv.length == 3 ) {
-  console.log(sl)
+if (!process.argv[2] || process.argv.length === 3) {
+  console.log(sl);
 } else {
   const l = Math.max.apply(null, process.argv.slice(2));
 
