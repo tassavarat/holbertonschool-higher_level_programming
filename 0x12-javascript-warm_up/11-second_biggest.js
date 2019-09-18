10fd923bb1b3d57a2bdbf38e6987080f68755ed6
Tim Assavarat <tassavarat@gmail.com>
2019-09-18 01:01:23 +0000

    Refactored code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
 /* Searches the second biggest int int list of arguments */
-const l = Math.max.apply(null, process.argv.slice(2));
 let sl = 0;
 
-for (let i = 2; process.argv[i]; ++i) {
-  if (sl < process.argv[i] && process.argv[i] < l) {
-    sl = process.argv[i];
+if (!process.argv[2] || process.argv.length == 3 ) {
+  console.log(sl)
+} else {
+  const l = Math.max.apply(null, process.argv.slice(2));
+
+  for (let i = 2; process.argv[i]; ++i) {
+    if (sl < process.argv[i] && process.argv[i] < l) {
+      sl = process.argv[i];
+    }
   }
+  console.log(String(sl));
 }
-console.log(String(sl));
