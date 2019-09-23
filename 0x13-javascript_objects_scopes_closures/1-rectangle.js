06bceddc2fbebe75f06aeb6378326ad0dc36c7a3
Tim Assavarat <tassavarat@gmail.com>
2019-09-23 20:40:28 +0000

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Creates a class Rectangle that defines a rectangle */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
