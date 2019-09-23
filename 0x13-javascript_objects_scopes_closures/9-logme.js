869b0f388890c2778eb2121688aba8fa13d87821
Tim Assavarat <tassavarat@gmail.com>
2019-09-23 23:20:12 +0000

    Moved variable position

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
+let count = 0;
 /**
  * Prints number of arguments already printed and the new argument value
  */
-let count = 0;
 exports.logMe = function (item) {
   console.log('%d: %s', count, item);
   ++count;
