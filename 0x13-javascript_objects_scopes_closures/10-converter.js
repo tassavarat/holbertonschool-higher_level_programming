3bf5d45b3f0d2c5ebd6b5542f374e682cdf17dbe
Tim Assavarat <tassavarat@gmail.com>
2019-09-23 23:49:35 +0000

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * converter - Converts number from base 10 to specified base
+ * @base: Base to convert to
+ *
+ * Return: Converted number
+ */
+exports.converter = function (base) {
+  return function (n) {
+    return (parseInt(n, 10).toString(base));
+  };
+};
