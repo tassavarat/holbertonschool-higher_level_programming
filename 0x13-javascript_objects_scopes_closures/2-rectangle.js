ac24ec5ff04497a536732f341294ba099bcb1d47
Tim Assavarat <tassavarat@gmail.com>
2019-09-23 20:49:58 +0000

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/** Creates a class Rectangle that defines a rectangle
+ * Creates an empty object if w or h is 0 or negative integer
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
