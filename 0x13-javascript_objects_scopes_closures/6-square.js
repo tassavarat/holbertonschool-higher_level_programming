5d3235dcfd5a9dec12d7dad41af4b84a924f01d3
Tim Assavarat <tassavarat@gmail.com>
2019-09-23 21:54:33 +0000

    First commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+/**
+ * Square class that defines square and inherits Rectangle of 5-square.js
+ * Instance method charPrint(c) prints rectangle using 'C', 'X' if undefined
+ */
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (C) {
+    if (C == null) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.width; ++i) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
