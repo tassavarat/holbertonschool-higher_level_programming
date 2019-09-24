3d0710af82c9efe64068dd9d5c29f2899352fdbc
Tim Assavarat <tassavarat@gmail.com>
2019-09-24 22:55:49 +0000

    First commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * Prints title of a Star Wars movie where number matches given integer
+ * First arg is episode number
+ */
+const request = require('request');
+
+request.get(`https://swapi.co/api/films/${process.argv[2]}`,
+  function (error, response, body) {
+    if (error) console.log(error);
+    console.log(JSON.parse(body).title);
+  });
