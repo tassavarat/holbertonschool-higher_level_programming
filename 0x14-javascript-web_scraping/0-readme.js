ab026a42c470048e607efa23f66bc2d29bd2fca5
Tim Assavarat <tassavarat@gmail.com>
2019-09-24 21:31:08 +0000

    Removed unecessary option

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-/* Reads and prints contents of a file */
+/**
+ * Reads and prints contents of a file */
+
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], function (err, data) {
   if (err) console.log(err);
   else process.stdout.write(data);
 });
