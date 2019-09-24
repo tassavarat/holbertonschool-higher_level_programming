a5a452d4be7d8bbf0b161474461766c71ba31529
Tim Assavarat <tassavarat@gmail.com>
2019-09-24 21:15:47 +0000

    Fixed new line removal

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) console.log(err);
-  else console.log(data.toString().replace(/\r?\n|\r/, ''));
+  else process.stdout.write(data.toString());
 });
