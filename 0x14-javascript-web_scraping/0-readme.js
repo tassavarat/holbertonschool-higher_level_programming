a500d50d4cc49e46890e097fb9f15407570a3f9f
Tim Assavarat <tassavarat@gmail.com>
2019-09-24 21:18:59 +0000

    Removed toString

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) console.log(err);
-  else process.stdout.write(data.toString());
+  else process.stdout.write(data);
 });
