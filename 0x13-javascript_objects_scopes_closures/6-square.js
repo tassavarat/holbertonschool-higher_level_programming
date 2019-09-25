762cae1b1809067a362a4609e1c9468f2ddca49b
Tim Assavarat <tassavarat@gmail.com>
2019-09-25 17:07:27 +0000

    Removed constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
+const pSquare = require('./5-square');
 
 /**
  * Square class that defines square and inherits Rectangle of 5-square.js
  * Instance method charPrint(c) prints rectangle using 'C', 'X' if undefined
  */
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends pSquare {
   charPrint (C) {
     if (C == null) {
       super.print();
