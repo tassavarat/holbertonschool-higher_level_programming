2d1cef5f9cec86f59373774e4844612b3253d0ea
Tim Assavarat <tassavarat@gmail.com>
2019-09-25 00:39:43 +0000

    Removed comments

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,10 +10,6 @@ let count = 0;
 request.get(process.argv[2], function (error, response, body) {
   if (error) console.log(error);
   else {
-    /*
-    console.log(JSON.parse(body).results[0].characters);
-    console.log('####');
-    */
     for (const film of JSON.parse(body).results) {
       for (const character of film.characters) {
         if (character.includes('18')) ++count;
