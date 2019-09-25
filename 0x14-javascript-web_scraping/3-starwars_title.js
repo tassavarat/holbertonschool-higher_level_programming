a7d990008ff13b6444b0dad33ab7c5ed0e1e9e41
Tim Assavarat <tassavarat@gmail.com>
2019-09-25 03:18:51 +0000

    Included ending forward slash

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@
  */
 const request = require('request');
 
-request.get(`https://swapi.co/api/films/${process.argv[2]}`,
+request.get(`https://swapi.co/api/films/${process.argv[2]}/`,
   function (error, response, body) {
     if (error) console.log(error);
     else console.log(JSON.parse(body).title);
