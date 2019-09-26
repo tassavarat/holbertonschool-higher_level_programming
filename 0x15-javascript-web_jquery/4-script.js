8de7a03c6563f480099a4b209b71300e865d5671
Tim Assavarat <tassavarat@gmail.com>
2019-09-26 23:06:15 +0000

    First commit

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+/**
+ * Toggles class of HTML tag HEADER between red and green when
+ * DIV#toggle_header clicked
+ */
+$('#toggle_header').click(function () {
+  $('header').toggleClass('red green');
+});
